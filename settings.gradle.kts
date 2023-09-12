6c13bd2b5fb5e505d915a8f7aa39fe2a013af752
Wanderlei Santos <wanderleisantosdias@gmail.com>
2023-09-12 11:02:24 -0300

    Created a project to study multi-platform Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MoviesAppK"
+include(":androidApp")
+include(":shared")
